b1ba62514f101d5f313570b6356803acb8552928
cameron64 <Cameron64@ymail.com>
2015-06-01 02:02:13 -0500

    Removed unnecessary get request

--- Controllers/gallary.js
+++ Controllers/gallary.js
@@ -1,12 +1,7 @@
 angular.module('cole').controller('gallary', function($scope,$http) {
 
-    $scope.pictures = [];
-    $http.get('pictureInfo.json').then(function (res) {
-        $scope.pictures = res.data;
-    });
     $http.get('heads.json').then(function (res) {
         $scope.heads = res.data;
+
     });
-
-
 });
